6f6e3e510c058860f8a07a41b4362bbe9f23778f
Gazzzolina <55655206+Gazzzolina@users.noreply.github.com>
2019-10-06 23:20:44 +0300

    Update apply.js

--- apply.js
+++ apply.js
@@ -1,3 +1,6 @@
 const apply = (num, func, arg) => {
-	return (num === 0) ? arg : apply (num-1, func, func(arg));
-} 
+  if (typeof func === 'function' && typeof num === 'number' && typeof arg === 'number') {
+    return (num === 0) ? arg : apply (num-1, func, func(arg));
+  }
+  return 'Check the input data!';
+}
